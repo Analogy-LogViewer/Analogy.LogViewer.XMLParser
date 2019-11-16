b1cc50686dfcc578b2e69fa4f38191291119547f
Lior Banai <liorbanai@gmail.com>
2019-11-16 18:14:50 +0200

    ininital skeleton project. nothing really exist here

---
+++ Analogy.LogViewer.XMLFileProvider/XMLParser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Analogy.LogViewer.XMLFileProvider
+{
+    public class XMLParser
+    {
+
+        public void ParserFile(string filename)
+        {
+            string xmlData = File.ReadAllText(filename);
+            Func<string, List<object>> parseData = (data) => ParserInternal(data);
+            try
+            {
+                parseData(xmlData);
+
+            }
+            catch (Exception e)
+            {
+                string data = TryFix(xmlData);
+                parseData(data);
+            }
+
+        }
+
+        private string TryFix(string data)
+        {
+            string corrrected = $"<Array>{data}</Array>";
+            return corrrected;
+        }
+
+        private List<object> ParserInternal(string data)
+        {
+            XDocument XMLDoc = XDocument.Parse(data);
+            return new List<object>();
+        }
+    }
+}
